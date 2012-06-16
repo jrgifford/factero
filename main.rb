2124b9db368d55388aea02493c637a9bbeef08ee
James Gifford <james@jamesrgifford.com>
2012-06-16 11:01:03 -0400

    Lets move away from system dependencies, and start moving towards say, the stdlib.

--- main.rb
+++ main.rb
@@ -1,5 +1,14 @@
 require 'green_shoes'
 require 'facter'
+require 'net/http'
+
+class WhatIsMy
+  def self.external_ip
+    url = "http://icanhazip.com/"
+    resp = Net::HTTP.get_response(URI.parse(url))
+    resp.body
+  end
+end
 
 Shoes.app( :title => "System Stats", :width => 350, :height => 250 ) do
   background linen
@@ -8,5 +17,5 @@ Shoes.app( :title => "System Stats", :width => 350, :height => 250 ) do
   para "Your IP Address is: " + Facter.ipaddress
   para "You are running: " + Facter.lsbdistdescription
   para "Your uptime is: " + Facter.uptime
-  para "Your external IP address is: " + `curl http://icanhazip.com/`
+  para "Your external IP address is: " + WhatIsMy::external_ip
 end
